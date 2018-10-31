53657c46bdc48daf25a6491abae957484d389531
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-10-30 18:02:24 -0700

    added hpFilter to app.js

--- js/components/app.js
+++ js/components/app.js
@@ -6,10 +6,10 @@ const pokemons = pokemonApi.getAll();
 
 pokemonsTable.init(pokemons);
 
-pokemonsFilter.init(function(nameFilter, type1Filter, type2Filter, attackFilter) {
+pokemonsFilter.init(function(nameFilter, type1Filter, type2Filter, attackFilter, hpFilter) {
     let filtered;
 
-    if(nameFilter || type1Filter || type2Filter || attackFilter) {
+    if(nameFilter || type1Filter || type2Filter || attackFilter || hpFilter) {
         nameFilter = nameFilter.toLowerCase();
     
         filtered = pokemons.filter(function(pokemon) {
@@ -17,10 +17,10 @@ pokemonsFilter.init(function(nameFilter, type1Filter, type2Filter, attackFilter)
             || pokemon.pokemon.toLowerCase().includes(nameFilter);
             const hasType1 = !type1Filter || pokemon.type_1.includes(type1Filter);
             const hasType2 = !type2Filter || pokemon.type_2.includes(type2Filter);
-           
             const hasAttack = !attackFilter || pokemon.attack > attackFilter;
+            const hasHP = !hpFilter || pokemon.hp > hpFilter;
 
-            return hasName && hasType1 && hasType2 && hasAttack;
+            return hasName && hasType1 && hasType2 && hasAttack && hasHP;
         });
     } else {
         filtered = pokemons;
--- js/components/pokemon-filter.js
+++ js/components/pokemon-filter.js
@@ -28,5 +28,13 @@ const pokemonsFilter = {
     }
 };
 
-
 export default pokemonsFilter;
+
+// // same as above, but with single listening function:
+
+        // function handleFilter() {
+        //     onFilter(nameInput.value, tagsInput.value);
+        // }
+        // nameInput.addEventListener('keyup', handleFilter);
+        // tagsInput.addEventListener('keyup', handleFilter);
+    }
